93e2a918cc9fe71546fdacad291e92fb55a7da47
balachandiran <kbcmca@gmail.com>
2014-01-21 17:17:01 +0530

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Bala on 21/01/14.
+//  Copyright (c) 2014 Ndot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Bala on 21/01/14.
+//  Copyright (c) 2014 Ndot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
